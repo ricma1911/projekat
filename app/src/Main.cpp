94f02a7af587783236285013f26fa0bad964796f
ricma1911 <ducamaric04@gmail.com>
2026-03-24 15:28:26 +0100

    app: inicijalizacija prozora aplikacije

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -1,8 +1,22 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/gtx/projection.hpp"
+
+
 #include <engine/core/Engine.hpp>
 
 /**
  * Start here...
  */
+
+
+class MojProjekat : public engine::core::App {
+public:
+
+    void app_setup() override {
+
+    };
+};
 int main(int argc, char **argv) {
-    return 0;
+    MojProjekat projekat;
+    return projekat.run(argc, argv);
 }
