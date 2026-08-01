4e406fff9d56407a88e1dab086659ea41ac0df37
ricma1911 <ducamaric04@gmail.com>
2026-08-01 18:18:39 +0200

    delete inappropriate street lamp model

--- app/include/MainController.hpp
+++ app/include/MainController.hpp
@@ -18,7 +18,6 @@ private:
     void update() override;
     void draw_car();
     void draw_side_objects();
-    void draw_steet_lamp();
     void draw_point_lamps();
     void draw_platform();
     void draw_skybox();
